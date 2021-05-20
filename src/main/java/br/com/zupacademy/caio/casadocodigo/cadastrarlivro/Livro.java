8b0504832f2985432dc5fa642d9d6870dec91ce5
Caio Rodrigues <caio.rodrigues@zup.com.br>
2021-05-20 19:35:30 -0300

    Funcionalidade de listar livros finalizada

--- src/main/java/br/com/zupacademy/caio/casadocodigo/cadastrarlivro/Livro.java
+++ src/main/java/br/com/zupacademy/caio/casadocodigo/cadastrarlivro/Livro.java
@@ -58,6 +58,9 @@ public class Livro {
 	@ManyToOne
 	private Autor autor;
 	
+	@Deprecated
+	public Livro() {}
+	
 	
 	public Livro(@NotBlank String titulo, @NotBlank @Size(max = 500) String resumo, @NotBlank String sumario,
 			@NotNull @Min(20) BigDecimal preco, @NotNull @Min(100) Integer numPaginas, @NotBlank String isbn,
@@ -71,6 +74,14 @@ public class Livro {
 		this.dataPublicacao = dataPublicacao;
 		this.categoria = categoria;
 		this.autor = autor;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
 	}
 
 	@Override
