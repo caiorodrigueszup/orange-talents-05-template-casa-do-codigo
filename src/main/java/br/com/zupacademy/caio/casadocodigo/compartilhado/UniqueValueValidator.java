dfd9a64e574515236175eb2b007703ac609c5394
Caio Rodrigues <caio.rodrigues@zup.com.br>
2021-05-20 15:53:02 -0300

    Funcionalidade cadastro de livro

--- src/main/java/br/com/zupacademy/caio/casadocodigo/compartilhado/UniqueValueValidator.java
+++ src/main/java/br/com/zupacademy/caio/casadocodigo/compartilhado/UniqueValueValidator.java
@@ -22,15 +22,19 @@ public class UniqueValueValidator implements ConstraintValidator<UniqueValue, Ob
 	public void initialize(UniqueValue params) {
 		domainAttribute = params.fieldName();
 		klass = params.domainClass();
+		
+		System.out.println(klass.getName());
 	}
 	
 	@Override
 	public boolean isValid(Object value, ConstraintValidatorContext context) {
-		Query query = manager.createQuery("select 1 from " + klass.getName() + " where " +  domainAttribute + " = :value");
+		Query query = manager.createQuery("select 1 from " + klass.getName() 
+											+ " where " +  domainAttribute + " = :value");
 		query.setParameter("value", value);
 		List<?> resultList = query.getResultList();
 		
-		Assert.state(resultList.size() <= 1, "Foi encontrado mais de um " + klass + " com o atributo " + domainAttribute + " = " + value);
+		Assert.state(resultList.size() <= 1, "Foi encontrado mais de um " + klass + 
+											" com o atributo " + domainAttribute + " = " + value);
 		
 		return resultList.isEmpty();
 	}
